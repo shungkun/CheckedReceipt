a1b262045ab3286aa508c10b0d6934cdf47b8369
Kun <Kun@DESKTOP-Q5QKDI0>
2022-12-04 18:32:18 +0800

    兌獎JS修改

--- ReceiptWeb/scripts/Receipt.js
+++ ReceiptWeb/scripts/Receipt.js
@@ -63,64 +63,60 @@ $(function () {
 
 function CheckedReceipt(Number) {
     Number = Number.PadLeft(8, '0');
+    var win = false;
     $.each(ReceiptResult, function (index, data) {
         var Issue = data.receiptYear + " 年 " + data.receiptMonth + " 月 "
         if (Number == data.specialNumber) {
             alert("恭喜中獎! " + Issue + " 特別獎: " + data.specialPrize + "元");
-            $("#InputNumber").select();
-            return false;
+            win = true;
         }
         if (Number == data.grandNumber) {
             alert("恭喜中獎! " + Issue + " 特獎: " + data.grandNumber + "元");
-            $("#InputNumber").select();
-            return false;
+            win = true;
         }
         if (Number == data.firstNumber_1
             || Number == data.firstNumber_2
             || Number == data.firstNumber_3) {
             alert("恭喜中獎! " + Issue + " 頭獎: " + data.firstPrize + "元");
-            $("#InputNumber").select();
-            return false;
+            win = true;
         }
         if (Number.substr(1) == data.firstNumber_1.substr(1)
             || Number.substr(1) == data.firstNumber_2.substr(1)
             || Number.substr(1) == data.firstNumber_3.substr(1)) {
             alert("恭喜中獎! " + Issue + " 二獎: " + data.secondPrize + "元");
-            $("#InputNumber").select();
-            return false;
+            win = true;
         }
         if (Number.substr(2) == data.firstNumber_1.substr(2)
             || Number.substr(2) == data.firstNumber_2.substr(2)
             || Number.substr(2) == data.firstNumber_3.substr(2)) {
             alert("恭喜中獎! " + Issue + " 三獎: " + data.thirdPrize + "元");
-            $("#InputNumber").select();
-            return false;
+            win = true;
         }
         if (Number.substr(3) == data.firstNumber_1.substr(3)
             || Number.substr(3) == data.firstNumber_2.substr(3)
             || Number.substr(3) == data.firstNumber_3.substr(3)) {
             alert("恭喜中獎! " + Issue + " 四獎: " + data.fourthPrize + "元");
-            $("#InputNumber").select();
-            return false;
+            win = true;
         }
         if (Number.substr(4) == data.firstNumber_1.substr(4)
             || Number.substr(4) == data.firstNumber_2.substr(4)
             || Number.substr(4) == data.firstNumber_3.substr(4)) {
             alert("恭喜中獎! " + Issue + " 五獎: " + data.fifthPrize + "元");
-            $("#InputNumber").select();
-            return false;
+            win = true;
         }
         if (Number.substr(5) == data.firstNumber_1.substr(5)
             || Number.substr(5) == data.firstNumber_2.substr(5)
-            || Number.substr(5) == data.firstNumber_3.substr(5)) {
+            || Number.substr(5) == data.firstNumber_3.substr(5))
+        {
             alert("恭喜中獎! " + Issue + " 六獎: " + data.sixthPrize + "元");
-            $("#InputNumber").select();
-            return false;
+            win = true;
         }
-        alert("沒有中獎! ");
-        $("#InputNumber").select();
-        return false;
     });
+    if (!win) {
+        alert("沒有中獎! ");
+    }
+    $("#InputNumber").select();
+    return false;
 
 }
 
